68061a7d2789b267c175366be9dc04fa3c1a3bc9
Judah Godden <339633265@GoTVDSB.ca>
2024-12-02 09:32:24 -0500

    Add project files.

---
+++ Snake/Game1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+
+public enum Direction
+{
+    Right,
+    Left, 
+    Up, 
+    Down
+}
+
+namespace Snake
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        List<Snake> snakes = new List<Snake>();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            Console.SetWindowSize(800, 496);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Snake
+{
+    public class Snake
+    {
+        private Rectangle _rect;
+        private Texture2D _texture;
+        private Direction _direction;
+
+        public Snake(Texture2D texture, Rectangle rect) 
+        { 
+            _texture = texture;
+            _rect = rect;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _rect, Color.White);
+        }
+
+        public void Update(float timer)
+        {
+            if (timer % 0.5 == 0)
+            {
+                //change next body to the previous direction of the one in front of it
+            }
+        }
+    }
+}
